980630558223ab794da3bf95d44845344a58bd03
Yalda <guocaoyi@gmail.com>
2023-10-08 00:08:04 +0800

    feat(sidepanel):vanilla support sidepanel

---
+++ template-vanilla-js/src/sidepanel/index.js
@@ -0,0 +1,18 @@
+import './index.css'
+
+const crx = 'create-chrome-ext'
+
+document.querySelector('#app').innerHTML = `
+  <main>
+  <h3>Side Panel Page!</h3>
+
+  <h6>v 0.0.0</h6>
+
+  <a
+    href="https://www.npmjs.com/package/create-chrome-ext"
+    target="_blank"
+  >
+    Power by ${crx}
+  </a>
+  </main>
+`
