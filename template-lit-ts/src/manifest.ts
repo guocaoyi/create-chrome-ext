d177c83b358c3641d766a97341cec7d026055283
ricosmall <ricosmaller@gmail.com>
2024-03-29 10:46:46 +0800

    fix: correct the size number of logo file name

--- template-lit-ts/src/manifest.ts
+++ template-lit-ts/src/manifest.ts
@@ -12,7 +12,7 @@ export default defineManifest({
   manifest_version: 3,
   icons: {
     16: 'img/logo-16.png',
-    32: 'img/logo-34.png',
+    32: 'img/logo-32.png',
     48: 'img/logo-48.png',
     128: 'img/logo-128.png',
   },
@@ -37,7 +37,7 @@ export default defineManifest({
   },
   web_accessible_resources: [
     {
-      resources: ['img/logo-16.png', 'img/logo-34.png', 'img/logo-48.png', 'img/logo-128.png'],
+      resources: ['img/logo-16.png', 'img/logo-32.png', 'img/logo-48.png', 'img/logo-128.png'],
       matches: [],
     },
   ],
--- template-react-ts/src/manifest.ts
+++ template-react-ts/src/manifest.ts
@@ -11,7 +11,7 @@ export default defineManifest({
   manifest_version: 3,
   icons: {
     16: 'img/logo-16.png',
-    32: 'img/logo-34.png',
+    32: 'img/logo-32.png',
     48: 'img/logo-48.png',
     128: 'img/logo-128.png',
   },
@@ -36,7 +36,7 @@ export default defineManifest({
   },
   web_accessible_resources: [
     {
-      resources: ['img/logo-16.png', 'img/logo-34.png', 'img/logo-48.png', 'img/logo-128.png'],
+      resources: ['img/logo-16.png', 'img/logo-32.png', 'img/logo-48.png', 'img/logo-128.png'],
       matches: [],
     },
   ],
