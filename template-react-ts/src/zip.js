8c3429b8e62c103898938c8909856310bfe27406
imo-tikuwa <imotikuwabathclean@gmail.com>
2024-09-02 20:15:09 +0900

    migrating from gulp 4 to 5

--- template-react-ts/src/zip.js
+++ template-react-ts/src/zip.js
@@ -5,6 +5,6 @@ const require = createRequire(import.meta.url)
 const manifest = require('../build/manifest.json')
 
 gulp
-  .src('build/**')
+  .src('build/**', { encoding: false })
   .pipe(zip(`${manifest.name.replaceAll(' ', '-')}-${manifest.version}.zip`))
   .pipe(gulp.dest('package'))
